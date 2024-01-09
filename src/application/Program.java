8bbc74be431553b64cb0a07fdbf6de2076ec5d82
Laura Perroni <lauraperroni2020@gmail.com>
2024-01-09 15:59:58 -0300

    criada classe Position

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+import entities.Position;
+
+class Program {
+
+	public static void main(String[] args) {
+
+		Position posi = new Position(2,3);
+		System.out.println(posi);
+	}
+
+}
